9fe875c00aa687c2402de27a62f4955c9e23c5b2
ripongoswami <sayangoswami49@gmail.com>
2026-07-04 10:47:50 -0400

    added code and test cases for read service

--- service/routes.py
+++ service/routes.py
@@ -68,8 +68,23 @@ def create_accounts():
 # READ AN ACCOUNT
 ######################################################################
 
-# ... place you code here to READ an account ...
-
+@app.route("/accounts/<int:account_id>", methods=["GET"])
+def read_account(account_id):
+    """
+    Reads an Account
+    This endpoint will read an Account based on the account_id that is requested
+    """
+    app.logger.info("Request to read an Account with id: %s", account_id)
+    
+    # 1. Find the account by ID
+    account = Account.find(account_id)
+    
+    # 2. Abort with 404 if not found
+    if not account:
+        abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
+        
+    # 3. Return the serialized data and 200 OK
+    return account.serialize(), status.HTTP_200_OK
 
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
