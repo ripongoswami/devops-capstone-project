873e261151b471f77e56cb40f73c96006ebcfe0d
ripongoswami <sayangoswami49@gmail.com>
2026-07-07 12:17:08 -0400

    fixed flake8 newline errors

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -215,3 +215,4 @@ class TestAccountService(TestCase):
         # Calling DELETE on the base URL (which only accepts POST or GET for list)
         resp = self.client.delete(BASE_URL)
         self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+        
